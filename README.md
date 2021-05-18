37e4c3a6ea0f88827af591ffba1893287efe7b86
ldasey <leonard.dasey@gmail.com>
2021-05-18 16:56:08 +0200

    update

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ The scripts require `Python 3` and the [Telethon](https://github.com/LonamiWebs/
 1. Sign up for Telegram
 2. Log in [here](https://my.telegram.org/auth), create an application and retrieve your API ID and hash
 3. Create a string session and retrieve your identification string which is described [here in Telethon documentation](https://docs.telethon.dev/en/latest/concepts/sessions.html)
-4. Enter these three identification credentials into the script
+4. Enter these three identification credentials into `config.py` which is then easily accessed through `import config` in the main scripts
+5. Create a .txt-file in the sub directory `/lists/`. One row should contain exactly one channel/group name in the case of `scrape_public.py` and one group invite ID in the case of `scrape_private.py`
+6. Create a subdirectory called `/log/` to which the log file will be saved
+7. Create a subdirectory called `/csv/` to which the output .csv-file will be saved
---
+++ scrape_public.py
@@ -0,0 +1,119 @@
+# import relevant libraries
+
+import sys
+import pandas as pd
+import telethon.utils as utils
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon.tl.functions.messages import CheckChatInviteRequest
+import logging
+import datetime
+import time
+
+
+# import sql und telegram login
+import config
+
+
+
+### configuration:
+
+#name of the logfile (.txt). Logfile is saved in sub directory /log/
+logname = 'wave2_public'
+
+#input for scaping: (.txt) One group/channel per line. File should be in sub directory /lists/
+listname = 'public'
+
+# .csv output (.csv). File will be saved to sub directory /csv/
+csvname = 'wave2_public'
+
+# invite link required? (1=yes)
+is_invite = 0
+
+# wave, useful if script is executed repeatedly
+wave = 2
+
+# sleep after loop (in secs). Time out is useful as to not make Telegram believe we are a bot
+sleep = 361
+
+
+
+#log: will be saved to sub directory /log/
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logger = logging.getLogger()
+logger.addHandler(logging.FileHandler('log/' + logname + '_' + datetime.datetime.now().strftime("%d.%m.%Y_%H.%M") + '.txt', 'a'))
+print = logger.info
+
+# check if script is executed rightfully, as .csv-file may be overwritten
+
+continuescript = input('Would you like to continue with this script? (Y/n)')
+
+if continuescript in ['Y', 'y']:
+    print('Great. lets continue')
+else:
+    sys.exit()
+
+
+
+time.sleep(5.5)
+
+# read in input for scraping
+lineList = [line.rstrip('\n') for line in open("lists/" + listname + '.txt')]
+
+chats = lineList
+
+
+# the actual scraping process
+
+client = TelegramClient(StringSession(config.string), config.api_id, config.api_hash)
+
+async def main():
+    for chat in chats:
+        try:
+            # try to join
+            await client(JoinChannelRequest(chat))
+            time.sleep(5)
+        except:
+            print(chat + ' channel/group does not exist or other error. Skipping to next channel...')
+        else:
+
+            chattitle = chat
+            time.sleep(10)
+            print('Successfully joined ' + chattitle + '. Starting scraping now...')
+            list = []
+            async for message in client.iter_messages(chattitle, reverse=True):
+                list.append([chattitle, message.chat_id, message.is_private, message.is_group, message.is_channel, is_invite, wave, message.id,
+                             message.sender_id, message.date, message.text, message.action])
+            df = pd.DataFrame(list)
+            df.columns = ['chat_title', 'chat_id', 'is_private', 'is_group', 'is_channel', 'is_invite', 'wave', 'message_id', 'message_senderid',
+                          'message_date', 'message_text', 'message_action']
+
+            df['message_action'] = df['message_action'].astype(str)
+
+        time.sleep(sleep)
+
+
+with client:
+    client.loop.run_until_complete(main())
+
+#time.sleep(5.5)
+
+
+print('Successfully scraped everything to table ' + tablename + '. Good job.')
+
+
+print('Saving file to .csv...')
+
+
+
+# save data to .csv-file. Can be removed if no .csv output is wished
+
+try:
+    df.to_csv('csv/' + csvname + '.csv', sep = '-')
+except:
+    print('Was not able to write .csv')
+else:
+    print('everything worked like a charm. .csv file saved.')
